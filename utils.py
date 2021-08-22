918c0acec0ae62f98cc5ad43028f5e18801e4d10
Rohan Debroy <debroy.debroy2017@gmail.com>
2021-08-22 09:45:35 +0530

    Added doc string

--- utils.py
+++ utils.py
@@ -4,9 +4,15 @@ import requests
 import pathlib
 import json
 
+#* This global variable is used to keep track of how many times we have retried.
+#*Basically a hacky method
 retry_counter = 0
 
 def load_config() -> dict:
+    '''
+    This function loads data from the config file else it calls the fetch config
+    and then save the config.
+    '''
     try:
         with open(f'{pathlib.Path.cwd()}/config.json', mode='r', encoding='utf-8') as file:
             data = json.load(file)
@@ -21,7 +27,11 @@ def load_config() -> dict:
 
 
 def fetch_config() -> dict:
-    headers = {
+    '''
+    This function requests for the configuration like accepted domains
+    and access key and other information and returns a json response. 
+    '''
+    headers = { 
         'x-requested-with': 'XMLHttpRequest',
         'x-meow': 'me\xBAow'
     }
@@ -34,13 +44,24 @@ def fetch_config() -> dict:
 
 
 def save_config(data: dict) -> bool:
+    '''
+    This function is used to save all the config fetched from the server
+    to a json file.
+
+    It is used to retain data so that unnecessary calls to the server can be reduced. 
+    '''
     try:
         with open(f'{pathlib.Path.cwd()}/config.json', mode='w', encoding='utf-8') as file:
             json.dump(data, file)
+        return True
     except:
         return False
 
 def fetch_request(url, access_key) -> dict:
+    '''
+    This function generates the headers and payload and send it
+    along with the request and returns a json response
+    '''
     headers = {
         'x-requested-with': 'XMLHttpRequest',
         'x-meow': 'me\xBAow',
@@ -66,6 +87,11 @@ def fetch_request(url, access_key) -> dict:
         return retry_request(url)
 
 def retry_request(url: str):
+    '''
+    A simple hack to retry my request for a total of 3 times
+    This function calls the initlaize request with the url
+    as params.
+    '''
     global retry_counter 
     retry_counter+= 1
         
@@ -75,23 +101,40 @@ def retry_request(url: str):
     return initalize_request(url)
 
 def initalize_request(url: str) -> dict:
+    '''
+    This function after initialization passes the access key and the url
+    to the fetch_request method which return the bypassed url.
+    '''
     data = load_config()
     access_key = decode_string(data["access_key"])
     return fetch_request(url=url, access_key=access_key)
 
 def is_url_supported(url: str) -> bool:
+    '''
+    This Function returns a boolean True if the url falls under supported category
+    else False is returned.
+    '''
     data = load_config()
     base_url = urlparse(url).netloc
 
     return base_url in data["diff_domains"] or base_url in data["accept_domains"]
 
 def bypass_url(url: str) -> dict:
+    '''
+    This function calls the initlaize method by passing the url as params
+    and returns a response.
+    '''
     if is_url_supported(url):
         return initalize_request(url=url)
     else:
         return {"status":"Url unsupported"}
 
 def recursive_bypass_url(url:str) -> dict:
+    '''
+    It calls bypass url underneath recursivelys i.e it will keep calling bypass url
+    on the provided url for the first time and the responsed url from the 2nd time 
+    unitl the actuall url is fetched.
+    '''
     response = bypass_url(url)
     try:
         check_supported_url = is_url_supported(response["result"])
--- utils_crypto.py
+++ utils_crypto.py
@@ -40,17 +40,5 @@ def decode_string(string: str) -> str:
     '''
     This function applies the below mentioned functions on a string in an order of 
     hextobin -> fix_padding -> decode_base64 -> decrypt_rot_47
-
-    :param string: Accepts hex encoded strings
-    :type string: str.
-
-    :return: : decoded string in utf-8 encoding
-
-    Usage::
-        >>>import utils_crypto
-        >>>string = "4e574a684e444e664e6d466f5a325a6c4d6d4a6c4e32..."
-        >>>utils_crypto.decode_string(string)
-        d32cb0e29876a36f6559c986354c4f74ed6f88d85cc69666a62f0fb93...
-
     '''
     return decrypt_rot_47(decode_base64(fix_padding_base64(hextobin(string))))
